72515a08f1a89987efb2c3e5659aca4212086156
THE VIRUS MIRACLES <88598722+THE-VIRUS-MIRACLES@users.noreply.github.com>
2022-01-14 14:34:21 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -17,11 +17,11 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━✨️Ameer Suhail✨️━━━━━⛦
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━𝘼   𝙅   ✯ ꯬     🎈𖤐━━━━━⛦
 
 
 𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 Ameer Suhail 𝐛𝐨𝐭.
-            *Pikachu*
+            *𝘼   𝙅   ✯ ꯬     🎈𖤐*
 
 ■□■□■□■□■□■□■□■□■□■□
 ♕ *𝙈𝙚𝙙𝙞𝙖 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨* ♕
@@ -134,8 +134,8 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
 🍙𝙐𝙨𝙚➜ Converts text into glowing sticker.
 ⚠️ex  attp pikachu
 ■□■□■□■□■□■□■□■□■□■□
-═════💢Pikachu💢═════
-▣▣▣▣▣▣▣▣▣ameersuhail▣▣▣▣▣▣▣▣▣▣▣
+═════💢𝘼   𝙅   ✯ ꯬     🎈𖤐💢═════
+▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣▣
 
 `}) 
 
